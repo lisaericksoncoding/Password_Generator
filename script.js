206e54d0189b318561240bd943059cb619da4350
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2022-09-15 22:57:51 -0500

    variables and first if

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var characters = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength =  
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var character = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var chooseLength = "";
+var chooseNumber;
+var chooseCharacter;
+var chooseLowercase;
+var chooseUppercase;
+
+function generatePassword () {
+  var chooseLength = (prompt("How many characters would you like your password to be?"));
+
+  if (chooseLength < 8 || chooseLength > 128) {
+    alert("Password must be at least 8 characters but no more than 128 characters. Resubmit.");
+    var chooseLength = (prompt("How many characters would you like your password to be?"));
+  }
+
+ alert("Great! Now you'll select which parameters you'd like your password to contain.")
+
+  var chooseNumber = (confirm("Would you like your password to contain numbers?"));
+  var chooseCharacter = (confirm("Would you like your password to contain special characters?"));
+  var chooseLowercase  = (confirm("Would you like your password to contain lowercase letters?"));
+  var chooseUppercase  = (confirm("Would you like your password to contain uppercase letters?"));
 
 // Write password to the #password input
 function writePassword() {
@@ -14,3 +37,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
