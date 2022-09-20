6f3e425c2730663aa2557c88c9e7fbba77e67688
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2022-09-19 23:13:55 -0500

    final version, readme updates, and screenshot images

---
+++ README.md
@@ -0,0 +1,44 @@
+# Password Generator
+
+Create a random password generator that provides you with a new, secure password after you select password length and criteria.
+
+## Screenshots
+
+<img src="./assets/images/passwordgenerator-landing-page" width =200 />
+<img src="./assets/images/passwordgenerator-prompts" width =200 />
+<img src="./assets/images/passwordgenerator-display" width =200 />
+
+## Links
+
+- [Repo](https://github.com/lisaericksoncoding/Password_Generator "Password Generator Repo")
+
+- [Live](https://lisaericksoncoding.github.io/Password_Generator "Live View")
+
+## Built With
+
+- HTML (provided code)
+- CSS (provided code)
+- Javascript
+- Visual Studio Code
+
+## Sources
+
+Stock images pulled from Pexel.com
+[Site](https://www.w3schools.com "W3Schools")
+[Site](https://stackoverflow.com/ "Stack Overflow")
+
+## Author
+
+**Lisa Erickson**
+
+- [Profile](https://github.com/lisaericksoncoding "Lisa Erickson")
+- [LinkedIn](https://www.linkedin.com/in/lisalerickson/ "Lisa Erickson")
+- [Email](mailto:erickson.l.lisa@gmail.com?subject=Feedback "Feedback")
+
+## License
+
+MIT License
+
+## 🤝 Support
+
+Feedback is welcome!
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Set variables
 var generateBtn = document.querySelector("#generate");
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var character = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var character = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var randomCombo = []
 
 var chooseLength = "";
 var chooseNumber;
@@ -11,7 +12,7 @@ var chooseCharacter;
 var chooseLowercase;
 var chooseUppercase;
 
-function generatePassword () {
+function getOptions() {
   var chooseLength = (prompt("How many characters would you like your password to be?"));
 
   if (chooseLength < 8 || chooseLength > 128) {
@@ -19,39 +20,46 @@ function generatePassword () {
     var chooseLength = (prompt("How many characters would you like your password to be?"));
   }
 
- alert("Great! Now you'll select which characters you'd like your password to contain.")
+  alert("Great! Now you'll select which characters you'd like your password to contain.")
 
   var chooseNumber = (confirm("Would you like your password to contain numbers? If yes, click OK"));
   var chooseCharacter = (confirm("Would you like your password to contain special characters? If yes, click OK"));
-  var chooseLowercase  = (confirm("Would you like your password to contain lowercase letters? If yes, click OK"));
-  var chooseUppercase  = (confirm("Would you like your password to contain uppercase letters? If yes, click OK"));
+  var chooseLowercase = (confirm("Would you like your password to contain lowercase letters? If yes, click OK"));
+  var chooseUppercase = (confirm("Would you like your password to contain uppercase letters? If yes, click OK"));
 
   if ((chooseNumber == false) && (chooseCharacter == false) && (chooseLowercase == false) && (chooseUppercase == false)) {
- alert("You must select at least one character type.")
+    alert("You must select at least one character type.")
 
- var chooseNumber = (confirm("Would you like your password to contain numbers? If yes, click OK"));
- var chooseCharacter = (confirm("Would you like your password to contain special characters? If yes, click OK"));
- var chooseLowercase  = (confirm("Would you like your password to contain lowercase letters? If yes, click OK"));
- var chooseUppercase  = (confirm("Would you like your password to contain uppercase letters? If yes, click OK"));
-} 
-
-function passwordContents () {
-var randomNumber= number[Math.floor(Math.random()*number.length)];
-var randomCharacter= character[Math.floor(Math.random()*character.length)];
-var randomLowercase= lowercase[Math.floor(Math.random()*lowercase.length)];
-var randomUppercase= uppercase[Math.floor(Math.random()*uppercase.length)];
-
-if (chooseNumber) {return randomNumber}
-if (chooseCharacter) {return randomCharacter} 
-if (chooseLowercase) {return randomLowercase} 
-if (chooseUppercase) {return randomUppercase} 
+    var chooseNumber = (confirm("Would you like your password to contain numbers? If yes, click OK"));
+    var chooseCharacter = (confirm("Would you like your password to contain special characters? If yes, click OK"));
+    var chooseLowercase = (confirm("Would you like your password to contain lowercase letters? If yes, click OK"));
+    var chooseUppercase = (confirm("Would you like your password to contain uppercase letters? If yes, click OK"));
+  }
+  var passwordOptions = { length: chooseLength, number: chooseNumber, specialCharacter: chooseCharacter, lowerCase: chooseLowercase, uppercase: chooseUppercase };
+  return passwordOptions
 }
 
-var randomCombo = randomNumber + randomCharacter + randomLowercase + randomUppercase;
-
-do {passwordContents ()} while (randomCombo < chooseLength);
-
-
+function generatePassword() {
+  var passwordOptions = getOptions()
+  var possibleCharatacters = []
+  var selectedCharacters = []
+  if (passwordOptions.number) {
+    possibleCharatacters = possibleCharatacters.concat(number)
+  }
+  if (passwordOptions.specialCharacter) {
+    possibleCharatacters = possibleCharatacters.concat(character)
+  }
+  if (passwordOptions.lowerCase) {
+    possibleCharatacters = possibleCharatacters.concat(lowercase)
+  }
+  if (passwordOptions.uppercase) {
+    possibleCharatacters = possibleCharatacters.concat(uppercase)
+  }
+  for (let i = 0; i < passwordOptions.length; i++) {
+    var randomPasswordCharacter = possibleCharatacters[Math.floor(Math.random() * possibleCharatacters.length)];
+    selectedCharacters.push(randomPasswordCharacter)
+  }
+  return selectedCharacters.join("")
 }
 
 // Write password to the #password input
